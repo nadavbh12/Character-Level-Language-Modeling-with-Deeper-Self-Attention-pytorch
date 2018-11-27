0c8a2d5bac3add54b6e2401c792d16c6b4479915
Nadav Bhonker <nadavbh@gmail.com>
2018-11-27 16:32:23 +0200

    removed export_onnx

--- main.py
+++ main.py
@@ -237,16 +237,6 @@ def train():
         if batch % 10000 == 0 and batch > 0:
             break
 
-
-def export_onnx(path, batch_size, seq_len):
-    print('The model is also exported in ONNX format at {}'.
-          format(os.path.realpath(args.onnx_export)))
-    model.eval()
-    dummy_input = torch.LongTensor(seq_len * batch_size).zero_().view(-1, batch_size).to(device)
-    hidden = model.init_hidden(batch_size)
-    torch.onnx.export(model, (dummy_input, hidden), path)
-
-
 # Loop over epochs.
 best_val_loss = None
 optimizer = optim.SGD(model.parameters(), args.lr, args.momentum)
@@ -282,7 +272,3 @@ print('=' * 89)
 print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
     test_loss, math.exp(test_loss)))
 print('=' * 89)
-
-if len(args.onnx_export) > 0:
-    # Export the model in ONNX format.
-    export_onnx(args.onnx_export, batch_size=1, seq_len=args.bptt)
